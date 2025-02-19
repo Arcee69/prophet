1af8cf2f95fcc19dc71454c7f85168cef3270375
Arcee69 <olawalearcee@gmail.com>
2025-02-19 16:52:01 +0100

    Project Setup

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.html", "./src/**/*.jsx", "./src/**/*.js"],
+  theme: {
+    screens: {
+      xs: "300px", //360px
+      sm: "640px",
+      md: "768px",
+      lm: "1024px",
+      lg: "1280px",
+      xl: "1536px",
+    },
+
+    fontFamily: {
+      nunito: ["Nunito Sans"],
+      inter: ["Inter"],
+      hanken: ["Hanken Grotesk"],
+    },
+    colors: {
+      primary: "#3BFF81",
+
+      secondary: "",
+
+      tertiary: "",
+
+      white: "#fff",
+      black: "#000",
+
+      MODAL_BACKGROUND: "rgba(11, 12, 14, 0.77)",
+
+      GREEN: {
+        _100: "#42D979",
+        _200: "#6C8173",
+      },
+
+      RED: {
+        _100: "#AF202D",
+      },
+    },
+  },
+  plugins: [],
+};
