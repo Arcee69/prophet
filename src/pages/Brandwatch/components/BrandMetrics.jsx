406aed71e3fff2ce41b750a43f2aa383b632bd36
Arcee69 <olawalearcee@gmail.com>
2026-08-02 16:56:45 +0100

    Chore: BrandWatch Update

--- src/pages/Brandwatch/components/BrandMetrics.jsx
+++ src/pages/Brandwatch/components/BrandMetrics.jsx
@@ -56,12 +56,12 @@ const BrandMetrics = ({
     return (
         <>
             <div ref={reportRef}>
-                <div className='grid grid-cols-3 gap-4'>
+                <div className='grid grid-cols-2 gap-4'>
                     {loading ? (
                         <>
                             <SkeletonCard />
                             <SkeletonCard />
-                            <SkeletonCard />
+                            {/* <SkeletonCard /> */}
                         </>
                     ) : (
                         <>
@@ -76,7 +76,7 @@ const BrandMetrics = ({
                                 </div>
                             </div>
 
-                            <div className='flex flex-col justify-between p-4 bg-white rounded-lg h-[200px] shadow-md'>
+                            {/* <div className='flex flex-col justify-between p-4 bg-white rounded-lg h-[200px] shadow-md'>
                                 <p className='font-jost text-2xl font-semibold text-[#252F3D]'>Total Engagement</p>
                                 <div className='flex items-center justify-between'>
                                     {engagementData?.map((item, index) => (
@@ -85,7 +85,7 @@ const BrandMetrics = ({
                                         </div>
                                     ))}
                                 </div>
-                            </div>
+                            </div> */}
 
                             <div className='flex flex-col justify-between p-4 bg-white rounded-lg h-[200px] shadow-md'>
                                 <p className='font-jost text-2xl font-semibold text-[#252F3D]'>Estimated  Reach</p>
@@ -512,7 +512,7 @@ const BrandMetrics = ({
                 )}
 
                 {/* Top Mentions */}
-                <div className='flex flex-col mt-10 gap-[11px]'>
+                <div className=' flex-col mt-10 hidden gap-[11px]'>
                     <div className='flex items-center gap-5'>
                         <p className='font-jost font-semibold text-[18px]  text-[#6B7280]'>
                             Top Conversations
